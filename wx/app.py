bc771602f3278c68ea14c8db63ddae81679fe545
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2014-05-18 15:40:07 -0500

    Tell Jinja to trim whitespace

--- wx/app.py
+++ wx/app.py
@@ -5,4 +5,8 @@ from flask_peewee.db import Database
 app = Flask(__name__)
 app.config.from_object('wx.config.Configuration')
 
+# Jinja should clean up whitespace:
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
 database = Database(app)
