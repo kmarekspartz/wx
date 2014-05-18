be12aaf468589915d2cd333ec83413a1a0fa51ea
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2014-05-18 15:40:50 -0500

    Add SeaSurf for flask_peewee to automatically add in CSRF tokens to the login page.

--- wx/app.py
+++ wx/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_peewee.db import Database
+from flask.ext.seasurf import SeaSurf
 
 
 app = Flask(__name__)
 app.config.from_object('wx.config.Configuration')
+
+csrf = SeaSurf(app)
 
 # Jinja should clean up whitespace:
 app.jinja_env.trim_blocks = True
