f3f58d365e9dbbd882cb2bcb86f3508ba43ebd55
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2014-05-18 15:42:45 -0500

    Make API read-only

--- wx/api.py
+++ wx/api.py
@@ -7,17 +7,18 @@ from wx.models import User, Location, Station, Report, Value
 
 
 # See: http://flask-peewee.readthedocs.org/en/latest/rest-api.html
+# Read-only for now. Use the following:
 # user_auth = UserAuthentication(auth)
+# admin_auth = AdminAuthentication(auth)
 
-admin_auth = AdminAuthentication(auth)
-api = RestAPI(app, default_auth=admin_auth)
+api = RestAPI(app)
 
 
 class UserResource(RestResource):
     exclude = ('password', 'email',)
 
 
-api.register(User, UserResource, auth=admin_auth)
+api.register(User, UserResource)
 api.register(Location)
 api.register(Station)
 api.register(Report)
